2d43e4583d52959084b50d19a0477f54b200a71c
SonicGD <sonicgd@gmail.com>
2016-10-20 13:56:39 +0500

    Change Assembly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModelPredicates")]
+[assembly: AssemblyProduct("Sitko.ModelSelector")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
