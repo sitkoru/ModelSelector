5a69894e10ec7428a53347fdbb57c304ff987f22
SonicGD <sonicgd@gmail.com>
2016-10-31 13:16:07 +0500

    add IQueryable extension

---
+++ Extensions/QueryableExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Sitko.ModelSelector.Base;
+
+namespace Sitko.ModelSelector.Extensions
+{
+    public static class QueryableExtension
+    {
+        public static IQueryable<T> ApplySelector<T>(this IQueryable<T> model, ModelSelector<T> selector)
+        {
+            if (selector != null)
+                model = selector.ApplyPredicates(model);
+            return model;
+        }
+    }
+}
